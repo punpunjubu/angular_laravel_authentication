42962c5fbc90e67dafbec36917286846d3402000
punpunjubu <thanyarat.mu@inet.co.th>
2019-04-01 15:40:29 +0700

    update creating components

---
+++ angular-frontend/src/app/components/password/response-reset/response-reset.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-response-reset',
+  templateUrl: './response-reset.component.html',
+  styleUrls: ['./response-reset.component.sass']
+})
+export class ResponseResetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
