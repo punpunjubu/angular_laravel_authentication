19e09cf027b384adb330e7499169ecfe6a63c822
punpunjubu <thanyarat.mu@inet.co.th>
2019-04-01 17:52:11 +0700

    update creating login

--- angular-frontend/src/app/components/login/login.component.ts
+++ angular-frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public form = {
+    email: null,
+    password: null
+  };
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+  onSubmit(loginForm) {
+    console.log('loginForm', loginForm);
+    console.log('form', this.form);
+    return this.http.post('http://localhost:8000/api/auth/login', this.form).subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
   }
 
 }
